9af1c5904ab8b96b728e46ea5eb6a20defaa465c
HJ0216 <sso01215@naver.com>
2023-01-04 15:21:46 +0900

    java-practice

---
+++ java/Ex3_3.java
@@ -0,0 +1,13 @@
+public class Ex3_3 {
+    public void main(String[] args) {
+        int num = 10;
+        String s = (num > 0) ? "양수" : "음수" 
+        
+        
+        
+        
+        System.out.println(s); 
+        );
+    }
+
+}
