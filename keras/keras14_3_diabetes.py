848ffee1234f05a35819530efb887b67e8ba4c2a
HJ0216 <sso01215@naver.com>
2023-01-04 20:01:59 +0900

    ml-practice

--- keras/keras14_3_diabetes.py
+++ keras/keras14_3_diabetes.py
@@ -18,30 +18,30 @@ datasets = load_diabetes()
 x = datasets.data
 y = datasets.target
 
+'''
 print(x.shape) # (442, 10)
 print(y.shape) # (442, )
-# print(datasets.feature_names) # ['age', 'sex', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6']
+
+print(datasets.feature_names)
+['age', 'sex', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6']
+
+'''
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y,
-    train_size=0.7,
+    train_size=0.7
 )
 
 # 2. model
 model = Sequential()
-model.add(Dense(100, input_dim=10))
-model.add(Dense(200))
-model.add(Dense(300))
-model.add(Dense(300))
-model.add(Dense(300))
-model.add(Dense(200))
-model.add(Dense(100))
+model.add(Dense(12, input_dim=10))
+model.add(Dense(8))
 model.add(Dense(1))
 
 
 # 3. compile and train
-model.compile(loss='mse')
-model.fit(x_train, y_train, epochs=1000, batch_size=10)
+model.compile(loss='mse', optimizer='adam')
+model.fit(x_train, y_train, epochs=200, batch_size=10)
 
 
 # 4. evaluate and predict
@@ -63,3 +63,10 @@ print("RMSE: ", RMSE(y_test, y_predict))
 
 r2 = r2_score(y_test, y_predict)
 print("R2: ", r2)
+
+
+
+'''
+RMSE:  52.858866615480345
+R2:  0.5106541423504969
+'''
