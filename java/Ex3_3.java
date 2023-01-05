e29fc3ee30f861fc1f69e76708493875040246f3
HJ0216 <sso01215@naver.com>
2023-01-05 09:36:17 +0900

    java-practice

--- java/Ex3_3.java
+++ java/Ex3_3.java
@@ -1,8 +1,9 @@
 public class Ex3_3 {
     public static void main(String[] args) {
         int num = 10;
-        // String s = (num > 0) ? "양수" : s2 = (num<0) ? "음수" : "0";
-                
+		String s = num == 0 ? "0" : (num > 0 ? "양수" : "음수"); 
+        
+        System.out.println(s);
     }
 
 }
---
+++ java/Ex3_6.java
@@ -0,0 +1,10 @@
+public class Ex3_6 {
+    public static void main(String[] args) {
+        int num = 24;
+        int num2 = num % 10;
+        int num3 = 10 - num2;
+
+        System.out.println(num3);
+
+    }
+}
