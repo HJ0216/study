f6d0106b6190d113f4f0fa68bebc49fd06ab7480
HJ0216 <sso01215@naver.com>
2023-01-05 15:05:16 +0900

    java-practice

---
+++ java/Ex4_1.java
@@ -0,0 +1,38 @@
+public class Ex4_1 {
+    public static void main (String[] args){
+        // 1. 
+        int x = 15;
+        if(x>10 && x<20){
+            System.out.println("true");
+        }
+
+        // 2.
+        char ch = ' ';
+        if(!(ch==' ')){
+        // 대입: =, 동일 여부: ==
+            System.out.println("true");
+        }
+
+        // 3.
+        char ch2 = 'x';
+        if(ch2 == 'x' || ch2 == 'X') {
+            System.out.println("true");
+        }
+
+        // 4.
+        char ch3 = '3';
+        if(ch3>='0' && ch3<='9'){
+            System.out.println("true");
+        }
+
+        // 5.
+        char ch4 = 'a';
+        if(ch4>=97 && ch4<=128) {
+            System.out.println("ture");
+        }
+
+        // 6.
+        int year = 2023;
+        if(){}
+    }
+}
