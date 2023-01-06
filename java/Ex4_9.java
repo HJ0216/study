69736717a52df73f715404be61a19e007f3156e8
HJ0216 <sso01215@naver.com>
2023-01-06 10:13:17 +0900

    java-pratice

---
+++ java/Ex4_9.java
@@ -0,0 +1,14 @@
+public class Ex4_9 {
+    public static void main(String[] args) {
+        String str = "12345";
+        int num = 0;
+
+        for(int i=0; i<str.length(); i++){
+            num += str.charAt(i)+0;
+        } System.out.println(num);
+
+
+
+
+    }
+}
