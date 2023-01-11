e0a9d0c73990e1102cd94eaaded055e0300100bf
HJ0216 <sso01215@naver.com>
2023-01-11 09:32:02 +0900

    ml-practice

--- keras/keras_will_be_deleted.py
+++ keras/keras_will_be_deleted.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
+
 from sklearn.datasets import load_wine
+from sklearn.datasets import load_boston
 
 # 1. Data
-datasets = load_wine()
+# datasets = load_wine()
 
-plt.gray()
-plt.matshow(datasets.images[100])
-plt.show()
+# plt.gray()
+# plt.matshow(datasets.images[100])
+# plt.show()
+
+
+      
