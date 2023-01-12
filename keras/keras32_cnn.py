01dceb1bb2779c79151e0695cc6974b17bb82d7a
HJ0216 <sso01215@naver.com>
2023-01-12 20:21:02 +0900

    ml-practice

--- keras/keras32_cnn.py
+++ keras/keras32_cnn.py
@@ -5,22 +5,23 @@ from tensorflow.keras.layers import Dense, Conv2D, Flatten
 model = Sequential()
 model.add(Conv2D(filters=10, kernel_size=(2,2),
                  input_shape=(5,5,1)))
-# 이미지 픽셀 수(5*5) 1개(흑백) 3개(컬러RPG) = filter 수
-# kernel_size = 이미지 블럭 생성(2*2)
+# 이미지 픽셀 수(5*5) 1개(흑백) 3개(컬러RPG)
+# kernel_size = 합을 연산할 이미지 블럭의 사이즈(2*2)
 # 1 layer 연산 후 5*5 -> 4*4 -> ... 연산량이 점점 줄어감
 # filter = 10: 5*5 흑백 이미지 1장을 10장으로 늘림
 # hyper-parameter tuning: filters, kernel_size, activation 등
-model.add(Conv2D(filters=5, kernel_size=(2,2)))
-# filter = 5: 4*4(블럭화 후 이미지 크기가 줄어듬) 흑백 이미지 1장을 5장으로 늘림
-# filter: Dense에서의 hidden output layer
-# 분류 타입으로 Dense를 이용하여 y값과 연결
-# column화 시키기(3*3*5=45) 45개의 특성을 갖는 n개의 이미지로 교육
 
-model.add(Flatten()) # 4차원 -> 2차원(3*3*5) -> 45
-# Conv2D를 Flatten 처리하는 이유: 이미지를 수치화 했을 때, 행렬 값이 아닌 수치화
+model.add(Conv2D(filters=5, kernel_size=(2,2)))
+# filter = 5: 4*4(블럭화 후 이미지 크기가 줄어듬) 흑백 이미지 1장을 5장을 쌓아둠
+# filter: Dense의 hidden output layer
+# 이미지를 수치화 했을 때, 행렬 값이 아닌 수치화 -> y값과 연결: Conv2D->Flatten
+model.add(Flatten()) # 4차원 -> 2차원(3*3*5) -> (, 45)
+# column화 시키기(3*3*5=45): 45개의 특성을 갖는 n개의 이미지로 교육
+
 model.add(Dense(10))
 # (임의) hidden 10 layer 처리
 model.add(Dense(1)) # 이미지의 최종 수치화
+
 model.summary()
 
 
