472dbf5130e6d897f6ad60913a160d390f2fee31
HJ0216 <sso01215@naver.com>
2023-01-17 15:56:00 +0900

    improve the acc on iris file

--- keras/keras28_hamsu07_iris.py
+++ keras/keras28_hamsu07_iris.py
@@ -57,7 +57,7 @@ model.compile(loss='categorical_crossentropy',
 
 earlyStopping = EarlyStopping(monitor='accuracy', mode='auto', patience=20, restore_best_weights=True, verbose=1)
 
-model.fit(x_train, y_train, epochs=500, batch_size=8,
+model.fit(x_train, y_train, epochs=1024, batch_size=4,
           validation_split=0.2,
           callbacks=[earlyStopping],
           verbose=1)
