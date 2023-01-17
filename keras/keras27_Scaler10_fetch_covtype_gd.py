4102455bec5cbae7a4041d69159a7b1bbe6e97e9
HJ0216 <sso01215@naver.com>
2023-01-17 19:50:39 +0900

    improce an acc on fetch file

--- keras/keras27_Scaler10_fetch_covtype_gd.py
+++ keras/keras27_Scaler10_fetch_covtype_gd.py
@@ -37,7 +37,7 @@ x_test = scaler.transform(x_test)
 # 2. Model Construction
 model = Sequential()
 model.add(Dense(64, activation='relu', input_shape=(54, )))
-model.add(Dense(64, activation='sigmoid'))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(32,activation='relu'))
 model.add(Dense(16,activation='linear'))
 model.add(Dense(7,activation='softmax'))
@@ -51,7 +51,7 @@ model.compile(loss='categorical_crossentropy',
 
 earlyStopping = EarlyStopping(monitor='val_loss', mode='min', patience=20, restore_best_weights=True, verbose=1)
 
-model.fit(x_train, y_train, epochs=100, batch_size=128,
+model.fit(x_train, y_train, epochs=128, batch_size=256,
           validation_split=0.2,
           callbacks=[earlyStopping],
           verbose=1)
