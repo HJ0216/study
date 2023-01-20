2136dc2f9d77f6d36297665f0e446a538dff23d7
HJ0216 <sso01215@naver.com>
2023-01-20 15:38:19 +0900

    using function model

--- keras/keras28_hamsu10_fetch_covtype_ohe.py
+++ keras/keras28_hamsu10_fetch_covtype_ohe.py
@@ -55,9 +55,9 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy']
               )
 
-earlyStopping = EarlyStopping(monitor='val_loss', mode='min', patience=20, restore_best_weights=True, verbose=1)
+earlyStopping = EarlyStopping(monitor='val_loss', mode='min', patience=32, restore_best_weights=True, verbose=1)
 
-model.fit(x_train, y_train, epochs=128, batch_size=256,
+model.fit(x_train, y_train, epochs=256, batch_size=512,
           validation_split=0.2,
           callbacks = [earlyStopping],
           verbose=1)
