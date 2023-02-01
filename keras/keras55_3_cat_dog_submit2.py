a267511f3100c6d94ada04aa39ea83ca6a2ebf88
HJ0216 <sso01215@naver.com>
2023-02-02 07:40:16 +0900

    ml-practice

--- keras/keras55_3_cat_dog_submit2.py
+++ keras/keras55_3_cat_dog_submit2.py
@@ -13,7 +13,6 @@ from sklearn.model_selection import train_test_split
 
 
 # 1. Data
-FAST_RUN = False
 IMAGE_WIDTH=128
 IMAGE_HEIGHT=128
 IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
@@ -24,7 +23,7 @@ categories = []
 for filename in filenames:
     category = filename.split('.')[0]
     if category == 'dog':
-        categories.append(1) # dog -> 1 추가
+        categories.append(1)
     else:
         categories.append(0)
 
@@ -42,12 +41,6 @@ validate_df = validate_df.reset_index(drop=True)
 total_train = train_df.shape[0]
 total_validate = validate_df.shape[0]
 batch_size=15
-
-FAST_RUN = False
-IMAGE_WIDTH=128
-IMAGE_HEIGHT=128
-IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
-IMAGE_CHANNELS=3
 
 train_datagen = ImageDataGenerator(
     rotation_range=15,
@@ -75,32 +68,26 @@ y_train = np.load('D:/_data/dogs-vs-cats/numpy/cat_dog_y_train.npy')
 x_val = np.load('D:/_data/dogs-vs-cats/numpy/cat_dog_x_val.npy')
 y_val = np.load('D:/_data/dogs-vs-cats/numpy/cat_dog_y_val.npy')
 
-print(x_train.shape, x_val.shape) # (15, 128, 128, 3) (15, 128, 128, 3)
-print(y_train.shape, y_val.shape) # (15, 2) (15, 2)
+print(x_train.shape, x_val.shape) # (20000, 128, 128, 3) (5000, 128, 128, 3)
+print(y_train.shape, y_val.shape) # (20000, 2) (5000, 2)
 
 
 # 2. Model
-FAST_RUN = False
-IMAGE_WIDTH=128
-IMAGE_HEIGHT=128
-IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
-IMAGE_CHANNELS=3
-
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+model.add(MaxPooling2D(pool_size=(3, 3)))
 model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(3, 3)))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(3, 3)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(32, activation='relu'))
+model.add(Dense(16, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(2, activation='softmax'))
 model.summary()
@@ -113,8 +100,8 @@ earlystop = EarlyStopping(monitor='val_acc', mode='max', patience=64,
                               verbose=1)
 
 hist = model.fit(x_train, y_train,
-                    epochs=512,
-                    batch_size=4,
+                    epochs=256,
+                    batch_size=16,
                     validation_data=(x_val, y_val),
                     callbacks=[earlystop],
                     verbose=1)
@@ -133,6 +120,16 @@ test_filenames = os.listdir("D:/_data/dogs-vs-cats/test2")
 test_df = pd.DataFrame({
     'filename': test_filenames
 })
+
+'''
+print(test_df)
+        filename
+0     test_a.jpg
+1     test_b.jpg
+
+print(test_df.shape[0]) # 2
+'''
+
 nb_samples = test_df.shape[0]
 
 test_gen = ImageDataGenerator(rescale=1./255)
@@ -144,12 +141,13 @@ test_generator = test_gen.flow_from_dataframe(
     y_col=None,
     class_mode=None,
     target_size=IMAGE_SIZE,
-    batch_size=batch_size,
+    batch_size=2, # total_test_image=2
     shuffle=False
 )
 
 # for submission
-predict = model.predict_generator(test_generator, steps=np.ceil(nb_samples/batch_size))
+predict = model.predict_generator(test_generator, steps=np.ceil(nb_samples/2))
+# If you have steps=30 and your batch size = 4, you will predict the results for only 120 examples.
 
 test_df['category'] = np.argmax(predict, axis=-1)
 
