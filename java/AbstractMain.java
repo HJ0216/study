a38e084b39368a9d97ec60201f67f24bfe1ea20f
HJ0216 <sso01215@naver.com>
2023-02-16 23:14:11 +0900

    Create AbstractMain.java

---
+++ java/AbstractMain.java
@@ -0,0 +1,21 @@
+public class AbstractMain extends AbstractTest {
+
+	@Override
+	public void setName(String name) {this.name = name;}
+	@Override
+	public void abstractMethod() {};
+
+	
+	public static void main(String[] args) {
+		
+//		AbstractTest at = new AbstractTest();
+		// abstract class: new 연산자를 통한 memory allocation, obj 생성 불가
+
+
+
+
+//		AbstractTest_sb at2 = new AbstractTest_sb();
+		// abstract class를 구현한 class를 활용
+
+	}
+}
