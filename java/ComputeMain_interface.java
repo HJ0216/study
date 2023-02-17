51623b984710d139a50476024e88d4ac2fc85fbf
HJ0216 <sso01215@naver.com>
2023-02-17 16:54:06 +0900

    Create ComputeMain_interface.java

---
+++ java/ComputeMain_interface.java
@@ -0,0 +1,8 @@
+public class ComputeMain_interface {
+
+	public static void main(String[] args) {
+		ComputeService computeService = new ComputeService();
+		computeService.menu();
+		System.out.println("Program Terminated");
+	}
+}
